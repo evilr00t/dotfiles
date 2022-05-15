714e01c25ac02cd07b44e25c236d9ae46e72e373
Karol Czeryna <k@e-dot.uk>
2022-05-15 21:49:36 +0200

    Rename script to setup_nvim.sh - makes more sense

--- setup_nvim.sh
+++ setup_nvim.sh
@@ -43,4 +43,4 @@ mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
 ln -s ~/.vim $XDG_CONFIG_HOME/nvim
 ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+nvim +PluginInstall +qall
