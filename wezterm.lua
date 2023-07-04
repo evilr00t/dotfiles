82e13438b6b08dc83ced5c8e7e8f76742e2409ba
Karol Czeryna <k@e-dot.uk>
2023-07-04 15:49:41 +0100

    Maximize wezterm window on startup

--- wezterm.lua
+++ wezterm.lua
@@ -5,11 +5,17 @@ local wezterm = require 'wezterm'
 -- This table will hold the configuration.
 local config = {}
 local act = wezterm.action
+local mux = wezterm.mux
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
   config = wezterm.config_builder()
 end
+
+wezterm.on('gui-startup', function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
 
 -- This is where you actually apply your config choices
 
