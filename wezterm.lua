625ba8016cb9629de7ee3d414b24858e0caee403
Karol Czeryna <k@e-dot.uk>
2023-08-03 09:40:33 +0100

    [WEZTERM] Reverse video cursor

--- wezterm.lua
+++ wezterm.lua
@@ -23,10 +23,12 @@ end)
 config.ssh_backend = "Ssh2"
 config.color_scheme = 'nightfox'
 config.line_height = 1.2
+config.force_reverse_video_cursor = true
 config.adjust_window_size_when_changing_font_size = false
 config.hide_tab_bar_if_only_one_tab = true
 config.font = wezterm.font 'FantasqueSansM Nerd Font'
 config.font_size = 13.0
+
 config.leader = { key = ' ', mods = 'CTRL', timeout_milliseconds = 1000 }
 config.window_decorations = "RESIZE"
 config.inactive_pane_hsb = { saturation = 0.8, brightness = 0.7 }
