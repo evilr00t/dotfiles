e56fa6df3cbeb7f5244d7476bc6353409d410969
Karol Czeryna <k@e-dot.uk>
2024-08-12 22:52:15 +0100

    update wezterm config

--- wezterm.lua
+++ wezterm.lua
@@ -4,8 +4,6 @@ local wezterm = require("wezterm")
 local config = {}
 local act = wezterm.action
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
@@ -16,9 +14,6 @@ end
 --   window:gui_window():maximize()
 -- end)
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
 config.ssh_backend = "Ssh2"
 config.force_reverse_video_cursor = true
 config.adjust_window_size_when_changing_font_size = false
@@ -34,6 +29,8 @@ config.window_decorations = "RESIZE"
 config.mouse_wheel_scrolls_tabs = false
 config.leader = { key = " ", mods = "CTRL", timeout_milliseconds = 1000 }
 config.inactive_pane_hsb = { saturation = 0.8, brightness = 0.7 }
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 30
 config.window_frame = { font = wezterm.font({ family = "Monaspace Krypton", weight = "Regular" }) }
 config.window_padding = {
 	left = 5,
@@ -85,30 +82,14 @@ config.keys = {
 }
 
 config.colors = {
-	-- The default text color
 	foreground = "#C4BDB5",
-	-- The default background color
 	background = "#1E1E1E",
-
-	-- Overrides the cell background color when the current cell is occupied by the
-	-- cursor and the cursor style is set to Block
 	cursor_bg = "#C02030",
-	-- Overrides the text color when the current cell is occupied by the cursor
 	cursor_fg = "#00ff00",
-	-- Specifies the border color of the cursor when the cursor style is set to Block,
-	-- or the color of the vertical or horizontal bar when the cursor style is set to
-	-- Bar or Underline.
 	-- cursor_border = '#52ad70',
-
-	-- the foreground color of selected text
 	-- selection_fg = 'black',
-	-- the background color of selected text
 	-- selection_bg = '#fffacd',
-
-	-- The color of the scrollbar "thumb"; the portion that represents the current viewport
 	-- scrollbar_thumb = '#222222',
-
-	-- The color of the split lines between panes
 	-- split = '#444444',
 
 	ansi = {
@@ -144,6 +125,35 @@ config.mouse_bindings = {
 		action = wezterm.action.CompleteSelection("Clipboard"),
 	},
 }
+-- NOTE: credits to https://alexplescan.com/posts/2024/08/10/wezterm/
+local function segments_for_right_status(window)
+	return {
+		window:active_workspace(),
+		"::",
+		wezterm.strftime("%a %b %-d %H:%M"),
+	}
+end
 
--- and finally, return the configuration to wezterm
+wezterm.on("update-status", function(window, _)
+	local segments = segments_for_right_status(window)
+
+	local color_scheme = window:effective_config().resolved_palette
+	-- Note the use of wezterm.color.parse here, this returns
+	-- a Color object, which comes with functionality for lightening
+	-- or darkening the colour (amongst other things).
+	local bg = wezterm.color.parse(color_scheme.background)
+	local fg = color_scheme.foreground
+
+	-- We'll build up the elements to send to wezterm.format in this table.
+	local elements = {}
+
+	for i, seg in ipairs(segments) do
+		table.insert(elements, { Background = { Color = bg } })
+		table.insert(elements, { Foreground = { Color = fg } })
+		table.insert(elements, { Text = " " .. seg .. " " })
+	end
+
+	window:set_right_status(wezterm.format(elements))
+end)
+
 return config
